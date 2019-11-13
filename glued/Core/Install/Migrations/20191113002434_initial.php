08208c19998755e4fdcf15239a45783940119475
Pavel Stratil <pavel@vaizard.org>
2019-11-13 00:28:48 +0000

    install updates

--- glued/Core/Install/Migrations/20191113002434_initial.php
+++ glued/Core/Install/Migrations/20191113002434_initial.php
@@ -1,9 +1,8 @@
 <?php
 
-use Phinx\Migration\AbstractMigration;
 use Phinx\Db\Adapter\MysqlAdapter;
 
-class Initial extends AbstractMigration
+class Initial extends Phinx\Migration\AbstractMigration
 {
     public function change()
     {
@@ -16,12 +15,25 @@ class Initial extends AbstractMigration
                 'comment' => 'Access credentials to users defined in t_core_users. Each user is allowed a secondary name/pass combination (plausible deniability)',
                 'row_format' => 'DYNAMIC',
             ])
+            ->addColumn('c_attr', 'json', [
+                'null' => true,
+                'comment' => 'Object attributes (status: enabled/disabled, allowed IPs, etc.)',
+            ])
+            ->addColumn('c_password', 'string', [
+                'null' => false,
+                'limit' => 255,
+                'collation' => 'utf8mb4_bin',
+                'encoding' => 'utf8mb4',
+                'comment' => 'Password',
+                'after' => 'c_attr',
+            ])
             ->addColumn('c_uid', 'integer', [
                 'null' => false,
                 'limit' => '10',
                 'signed' => false,
                 'identity' => 'enable',
                 'comment' => 'Unique row/object id',
+                'after' => 'c_password',
             ])
             ->addColumn('c_user_id', 'integer', [
                 'null' => false,
@@ -30,19 +42,6 @@ class Initial extends AbstractMigration
                 'comment' => 'Corresponds to t_core_users.c_uid',
                 'after' => 'c_uid',
             ])
-            ->addColumn('c_password', 'string', [
-                'null' => false,
-                'limit' => 255,
-                'collation' => 'utf8mb4_bin',
-                'encoding' => 'utf8mb4',
-                'comment' => 'Password',
-                'after' => 'c_user_id',
-            ])
-            ->addColumn('c_attr', 'json', [
-                'null' => true,
-                'comment' => 'Object attributes (status: enabled/disabled, allowed IPs, etc.)',
-                'after' => 'c_password',
-            ])
         ->addIndex(['c_uid'], [
                 'name' => 'c_uid',
                 'unique' => true,
@@ -50,6 +49,35 @@ class Initial extends AbstractMigration
         ->addIndex(['c_user_id'], [
                 'name' => 'c_user_id',
                 'unique' => false,
+            ])
+            ->create();
+        $this->table('t_core_profiles', [
+                'id' => false,
+                'primary_key' => ['c_uid'],
+                'engine' => 'InnoDB',
+                'encoding' => 'utf8mb4',
+                'collation' => 'utf8mb4_0900_ai_ci',
+                'comment' => 'Users\' system profiles (personal, invoicing, etc.)',
+                'row_format' => 'DYNAMIC',
+            ])
+            ->addColumn('c_json', 'json', [
+                'null' => false,
+                'comment' => 'Profile data',
+            ])
+            ->addColumn('c_uid', 'integer', [
+                'null' => false,
+                'limit' => '10',
+                'signed' => false,
+                'identity' => 'enable',
+                'comment' => 'Unique row id',
+                'after' => 'c_json',
+            ])
+            ->addColumn('c_users_uid', 'integer', [
+                'null' => false,
+                'limit' => '10',
+                'signed' => false,
+                'comment' => 'Profile owner',
+                'after' => 'c_uid',
             ])
             ->create();
         $this->table('t_core_users', [
@@ -61,17 +89,9 @@ class Initial extends AbstractMigration
                 'comment' => 'Users\' table, their profile and their attributes',
                 'row_format' => 'DYNAMIC',
             ])
-            ->addColumn('c_uid', 'integer', [
-                'null' => false,
-                'limit' => '10',
-                'signed' => false,
-                'identity' => 'enable',
-                'comment' => 'Unique row/object id',
-            ])
             ->addColumn('c_attr', 'json', [
                 'null' => true,
                 'comment' => 'Account attributes (enabled/disabled, GDPR anonymised, etc.)',
-                'after' => 'c_uid',
             ])
             ->addColumn('c_email', 'string', [
                 'null' => false,
@@ -81,31 +101,39 @@ class Initial extends AbstractMigration
                 'comment' => 'Primary email',
                 'after' => 'c_attr',
             ])
-            ->addColumn('c_screenname', 'string', [
-                'null' => false,
-                'limit' => 255,
-                'collation' => 'utf8mb4_0900_ai_ci',
-                'encoding' => 'utf8mb4',
-                'comment' => 'User\'s Visible screen name (nickname)',
-                'after' => 'c_email',
-            ])
             ->addColumn('c_lang', 'char', [
                 'null' => true,
                 'limit' => 5,
                 'collation' => 'utf8mb4_0900_ai_ci',
                 'encoding' => 'utf8mb4',
                 'comment' => 'Preferred language',
-                'after' => 'c_screenname',
+                'after' => 'c_email',
+            ])
+            ->addColumn('c_screenname', 'string', [
+                'null' => false,
+                'limit' => 255,
+                'collation' => 'utf8mb4_0900_ai_ci',
+                'encoding' => 'utf8mb4',
+                'comment' => 'User\'s Visible screen name (nickname)',
+                'after' => 'c_lang',
             ])
             ->addColumn('c_ts_created', 'timestamp', [
                 'null' => true,
                 'comment' => 'Timestamp: account created',
-                'after' => 'c_lang',
+                'after' => 'c_screenname',
             ])
             ->addColumn('c_ts_modified', 'timestamp', [
                 'null' => true,
                 'comment' => 'Timestamp: account modified',
                 'after' => 'c_ts_created',
+            ])
+            ->addColumn('c_uid', 'integer', [
+                'null' => false,
+                'limit' => '10',
+                'signed' => false,
+                'identity' => 'enable',
+                'comment' => 'Unique row/object id',
+                'after' => 'c_ts_modified',
             ])
         ->addIndex(['c_uid'], [
                 'name' => 'c_uid',
