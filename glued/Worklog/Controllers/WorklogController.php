c8e129abaa97020b324c3f9a5025d4c833d488bb
Pavel Stratil <pavel@vaizard.org>
2020-01-08 21:30:30 +0000

    json schema validation stub

--- glued/Worklog/Controllers/WorklogController.php
+++ glued/Worklog/Controllers/WorklogController.php
@@ -31,10 +31,42 @@ class WorklogController extends AbstractTwigController
         ]);
     }
 
+           public function converter(&$value, $key) {
+                if(gettype($value) === 'boolean'){
+                    $value = (bool) $value;
+                }
+            }
+
     public function me_post(Request $request, Response $response, array $args = []): Response
     {
+        // start off with the request body
+        $data = $request->getParsedBody();
+        // add metadata
+        $data['id'] = 0;
+        $data['_v'] = 1;
+        $data['_s'] = 'worklog/work';
+        //echo $_SESSION['core_user_id'];
+        // coerce types
+        if ( isset($data['team']) and is_array($data['team'])) { foreach ($data['team'] as $key => $val) { $data['team'][$key] = (int)$val; } }
+        if (isset($data['private'])) { $data['private'] = (bool) $data['private']; }
+        print_r($data);
+        // convert bodyay to object
+        $data = json_decode(json_encode((object)$data));
+        // TODO replace manual coercion above with a function to recursively cast types of object values according to the json schema object (see below)       
+    
+        // load the json schema and validate data against it
+        $loader = new \Opis\JsonSchema\Loaders\File("schema://worklog/", [
+            __ROOT__ . "/glued/Worklog/Controllers/Schemas/",
+        ]);
+        $schema = $loader->loadSchema("schema://worklog/work.v1.schema");
+        $validator = new \Opis\JsonSchema\Validator;
+        $result = $validator->schemaValidation($data, $schema);
+
+
+        print("<pre>".print_r($result,true)."</pre>");
         
-        //echo $_SESSION['core_auth_id'];
-        return $response->withJson($request->getParsedBody());
+        print_r($data);
+        //return $response->withJson($arr);
+        return $response;
     }
 }
