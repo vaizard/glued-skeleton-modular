77e3b4939906edd149100d9e8f1ddcb0a6e8172e
Pavel Stratil <pavel@vaizard.org>
2020-04-25 20:55:32 +0000

    yet another migration work

--- glued/Core/Install/Migrations/20200406211557_assets_items.php
+++ glued/Core/Install/Migrations/20200406211557_assets_items.php
@@ -2,28 +2,32 @@
 
 use Phinx\Db\Adapter\MysqlAdapter;
 
-class AssetItems extends Phinx\Migration\AbstractMigration
+class AssetsItems extends Phinx\Migration\AbstractMigration
 {
     public function change()
     {
         $this->execute('SET unique_checks=0; SET foreign_key_checks=0;');
         $this->table('t_assets_items', [
                 'id' => false,
+                'primary_key' => ['c_uid'],
                 'engine' => 'InnoDB',
                 'encoding' => 'utf8mb4',
                 'collation' => 'utf8mb4_unicode_ci',
-                'comment' => '',
+                'comment' => 'Assets items database',
                 'row_format' => 'DYNAMIC',
             ])
             ->addColumn('c_uid', 'integer', [
                 'null' => false,
                 'limit' => MysqlAdapter::INT_REGULAR,
+                'identity' => 'enable',
+                'comment' => 'Unique row/object id',
             ])
-            ->addColumn('c_data', 'json', [
+            ->addColumn('c_json', 'json', [
                 'null' => false,
                 'after' => 'c_uid',
+                'comment' => 'Assets item data',
             ])
-            ->addColumn('stor_name', 'string', [
+            ->addColumn('c_stor_name', 'string', [
                 'null' => true,
                 'limit' => 255,
                 'collation' => 'utf8mb4_unicode_ci',
@@ -31,6 +35,7 @@ class AssetItems extends Phinx\Migration\AbstractMigration
                 'after' => 'c_data',
             ])
             ->create();
+        $this->execute("ALTER TABLE t_assets_items DROP c_stor_name; ALTER TABLE t_assets_items ADD c_stor_name VARCHAR(255) GENERATED ALWAYS AS (json_unquote(json_extract(c_json,'$.name'))) VIRTUAL NOT NULL AFTER c_json;");
         $this->execute('SET unique_checks=1; SET foreign_key_checks=1;');
     }
 }
--- glued/Core/Install/Migrations/20200425204954_contacts_items.php
+++ glued/Core/Install/Migrations/20200425204954_contacts_items.php
@@ -2,18 +2,18 @@
 
 use Phinx\Db\Adapter\MysqlAdapter;
 
-class WorklogInitial extends Phinx\Migration\AbstractMigration
+class ContactsItems extends Phinx\Migration\AbstractMigration
 {
     public function change()
     {
         $this->execute('SET unique_checks=0; SET foreign_key_checks=0;');
-        $this->table('t_worklog_items', [
+        $this->table('t_contacts_items', [
                 'id' => false,
                 'primary_key' => ['c_uid'],
                 'engine' => 'InnoDB',
                 'encoding' => 'utf8mb4',
                 'collation' => 'utf8mb4_0900_ai_ci',
-                'comment' => 'Sellers are entities who sell stuff at specified conditions (think aliexpress or amazon sellers)',
+                'comment' => 'Contacts items',
                 'row_format' => 'DYNAMIC',
             ])
             ->addColumn('c_domain_id', 'integer', [
@@ -22,19 +22,25 @@ class WorklogInitial extends Phinx\Migration\AbstractMigration
             ])
             ->addColumn('c_json', 'json', [
                 'null' => false,
-                'comment' => 'Worklog item data (json document)',
+                'comment' => 'Contacts item data (json document)',
                 'after' => 'c_domain_id',
+            ])
+            ->addColumn('c_stor_name', 'string', [
+                'null' => false,
+                'limit' => 255,
+                'collation' => 'utf8mb4_0900_ai_ci',
+                'encoding' => 'utf8mb4',
+                'after' => 'c_json',
             ])
             ->addColumn('c_ts_created', 'timestamp', [
                 'null' => false,
                 'default' => 'CURRENT_TIMESTAMP',
                 'comment' => 'Timestamp created',
-                'after' => 'c_json',
+                'after' => 'c_stor_name',
             ])
             ->addColumn('c_ts_updated', 'timestamp', [
                 'null' => false,
                 'default' => 'CURRENT_TIMESTAMP',
-                'update' => 'CURRENT_TIMESTAMP',
                 'comment' => 'Timestamp updated',
                 'after' => 'c_ts_created',
             ])
@@ -45,39 +51,16 @@ class WorklogInitial extends Phinx\Migration\AbstractMigration
                 'comment' => 'Unique row id',
                 'after' => 'c_ts_updated',
             ])
-            ->addColumn('c_user_id', 'integer', [
-                'null' => false,
-                'limit' => MysqlAdapter::INT_REGULAR,
-                'comment' => 'Creator of the worklog item (account)',
-                'after' => 'c_uid',
-            ])
-            ->addColumn('c_stor_name', 'string', [
-                'null' => true,
-                'limit' => 255,
-                'collation' => 'utf8mb4_unicode_ci',
-                'encoding' => 'utf8mb4',
-                'after' => 'c_json',
-            ])
             ->addIndex(['c_domain_id'], [
                 'name' => 'c_domain_id',
                 'unique' => false,
             ])
-            ->addIndex(['c_user_id'], [
-                'name' => 'c_user_id',
-                'unique' => false,
-            ])
-            ->addForeignKey('c_user_id', 't_core_users', 'c_uid', [
-                'constraint' => 't_worklog_items_ibfk_1',
-                'update' => 'NO_ACTION',
-                'delete' => 'CASCADE',
-            ])
             ->addForeignKey('c_domain_id', 't_core_domains', 'c_uid', [
-                'constraint' => 't_worklog_items_ibfk_2',
+                'constraint' => 't_contacts_items_ibfk_1',
                 'update' => 'NO_ACTION',
                 'delete' => 'CASCADE',
             ])
             ->create();
-        $this->execute("ALTER TABLE t_worklog_items DROP c_stor_name; ALTER TABLE t_worklog_items ADD c_stor_name VARCHAR(255) GENERATED ALWAYS AS (json_unquote(json_extract(c_json,'$.id'))) VIRTUAL NOT NULL AFTER c_user_id;");
         $this->execute('SET unique_checks=1; SET foreign_key_checks=1;');
     }
 }
