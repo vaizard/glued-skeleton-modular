1dc2a9ec20d06fd6038a67bd80383b769c97cdc8
Pavel Stratil <pavel@vaizard.org>
2020-07-17 07:05:07 +0000

    quickfixing #38

--- glued/environment.php
+++ glued/environment.php
@@ -3,10 +3,14 @@ declare(strict_types=1);
 
 use Respect\Validation\Validator as v;
 
-// tell Respect\Validation where to look for classes extending its built-in rules set.
+// Ensure $HTTP_RAW_POST_DATA is deprecated warning does not appear
+ini_set('always_populate_raw_post_data','-1');
+
+// Tell Respect\Validation where to look for classes extending its built-in rules set.
 v::with('Glued\\Core\\Classes\\Validation\\Rules\\');
 $settings = $container->get('settings');
 
+// Set up error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', $settings['displayErrorDetails'] ? 'true' : 'false');
 ini_set('display_startup_errors', $settings['displayErrorDetails'] ? 'true' : 'false');
