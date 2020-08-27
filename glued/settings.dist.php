820a5117b84b84ac99343608b8d604676ec9ec42
Pavel Stratil <pavel@vaizard.org>
2020-08-27 09:38:23 +0000

    get rid of cli runtime errors due to undefined ['SERVER_NAME']

--- glued/settings.dist.php
+++ glued/settings.dist.php
@@ -141,9 +141,9 @@ return [
         'content-type-options' => 'nosniff',
         // TODO remove unsafe-eval once odan/twig-assets works with csp
         'csp' => [
-            'script-src' => [ 'self' => true, 'allow' => [ $_SERVER['SERVER_NAME'] ], 'strict-dynamic' => true, 'unsafe-eval' => true ],
+            'script-src' => [ 'self' => true, 'allow' => [ $_SERVER['SERVER_NAME'] ?? 'cli-run-or-unsupported-webserver'], 'strict-dynamic' => true, 'unsafe-eval' => true ],
             'object-src' => [ 'default-src' => 'false' ],
-            'frame-ancestors' => [ 'self' => true, 'allow' => [ 'https://' . $_SERVER['SERVER_NAME'] ] ],
+            'frame-ancestors' => [ 'self' => true, 'allow' => [ 'https://' . ( $_SERVER['SERVER_NAME'] ?? 'cli-run-or-unsupported-webserver' ) ] ],
             'base-uri' => 'self',
             'require-trusted-types-for' => 'script' // TODO not yet supported https://github.com/paragonie/csp-builder/issues/47
         ],
