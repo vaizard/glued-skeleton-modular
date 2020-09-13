9fef814f87d8c4dc23d7f3f30b1017031f217dd0
Pavel Stratil <pavel@vaizard.org>
2020-09-13 23:55:50 +0000

    rework monolog, add iso currencies, add json_decode filter to twig

--- glued/container.php
+++ glued/container.php
@@ -1,5 +1,6 @@
 <?php
 
+use Alcohol\ISO4217;
 use DI\Container;
 use Glued\Core\Classes\Auth\Auth;
 use Glued\Core\Middleware\TranslatorMiddleware;
@@ -25,6 +26,7 @@ use Symfony\Component\Translation\IdentityTranslator;
 use Symfony\Component\Translation\Loader\MoFileLoader;
 use Symfony\Component\Translation\Translator;
 use Twig\Loader\FilesystemLoader;
+use Twig\TwigFilter;
 use voku\helper\AntiXSS;
 
 
@@ -32,7 +34,7 @@ $container->set('settings', function() {
     return require_once(__ROOT__ . '/glued/settings.php');
 });
 
-$container->set(LoggerInterface::class, function (Container $c) {
+$container->set('logger', function (Container $c) {
     $settings = $c->get('settings')['logger'];
     $logger = new Logger($settings['name']);
     $processor = new UidProcessor();
@@ -95,6 +97,9 @@ $container->set('view', function (Container $c) {
     // Add twig exensions here
     $twig->addExtension(new TwigAssetsExtension($environment, (array)$c->get('settings')['assets']));
     $twig->addExtension(new TwigTranslationExtension($c->get(Translator::class)));
+    $environment->addFilter(new TwigFilter('json_decode', function ($string) {
+        return json_decode($string);
+    }));
     return $twig;
 });
 
@@ -119,6 +124,10 @@ $container->set(TranslatorMiddleware::class, static function (Container $contain
     return new TranslatorMiddleware($translator, $localPath);
 });
 
+
+$container->set('iso4217', function() {
+    return new Alcohol\ISO4217();
+});
 
 // *************************************************
 // GLUED CLASSES ***********************************
