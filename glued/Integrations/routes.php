3b3644b841a0c3901bceb0a379866ea007c6895b
Jakub Benes <pohadkar31@seznam.cz>
2021-01-07 19:21:54 +0000

    google sheets integration

---
+++ glued/Integrations/routes.php
@@ -0,0 +1,22 @@
+<?php
+use Glued\Integrations\Controllers\IntegrationsController;
+use Glued\Core\Middleware\RedirectGuests;
+use Glued\Core\Middleware\RestrictGuests;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Routing\RouteCollectorProxy;
+
+// Define the app routes.
+$app->group('/api/integrations/v1', function (RouteCollectorProxy $group) {
+    $group->get ('/google[/{uid:[0-9]+}]', IntegrationsController::class . ':google_list')->setName('integrations.google.api01');
+    $group->post ('/google[/{uid:[0-9]+}]', IntegrationsController::class . ':google_post');
+    $group->patch('/google[/{uid:[0-9]+}]', IntegrationsController::class . ':google_patch');
+    $group->delete('/google[/{uid:[0-9]+}]', IntegrationsController::class . ':google_delete');
+    $group->post ('/google/next[/{uid:[0-9]+}]', IntegrationsController::class . ':google_progress_next')->setName('integrations.google.next.api01');
+})->add(RestrictGuests::class);
+
+$app->group('/integrations', function (RouteCollectorProxy $group) {
+    $group->get ('/google/list', IntegrationsController::class . ':google_list_ui')->setName('integrations.google.list');
+    $group->get ('/google/detail[/{uid:[0-9]+}]', IntegrationsController::class . ':google_detail_ui')->setName('integrations.google.detail');
+})->add(RedirectGuests::class);
+
